4bb387950301487df6f96d6b3600dafc4627561a
Robert Vásquez <vasquezrober09@gmail.com>
2023-05-31 20:25:20 -0400

    First express server and test routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello desde SRC");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Im Server');
+});
+
+app.get('/new-route', (req, res) =>{
+  res.send('Hello, Im from new route');
+});
+
+app.get('/products', (req, res) =>{
+  res.json({
+    product: 'Boat',
+    price: 1000
+  });
+});
+
+app.listen(port, () => {
+  console.log('listen in port ' + port)
+});
