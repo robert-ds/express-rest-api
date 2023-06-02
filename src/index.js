a4b865c12886ae806f3274d883de73a6f0749a5b
Robert Vásquez <vasquezrober09@gmail.com>
2023-06-02 19:39:25 -0400

    feat: :sparkles: use fake data
    use package @faker-js/faker for dynamic endpoints

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {faker} = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
@@ -10,13 +11,57 @@ app.get('/new-route', (req, res) =>{
   res.send('Hello, Im from new route');
 });
 
+app.get('/users', (req, res) => {
+  const {limit, offset} = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }else {
+    res.send('Dont params');
+  }
+});
+
+app.get('/products/filter', (req, res) => {
+  res.send('Im Filter');
+});
+
 app.get('/products', (req, res) =>{
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for(let i = 0; i < limit; i++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(500,500,'auto')
+    });
+  }
+
+  res.json(products);
+});
+
+app.get('/product/:id', (req, res) => {
+  const {id} =  req.params;
+  res.json([
+    {
+      id,
+      product: 'Boat',
+      price: 1000
+    }
+  ]);
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
   res.json({
-    product: 'Boat',
-    price: 1000
+    categoryId,
+    productId
   });
 });
 
 app.listen(port, () => {
-  console.log('listen in port ' + port)
+  console.log('listen in port ' + port);
 });
