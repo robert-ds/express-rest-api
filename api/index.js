47588b8fb9cafd6c470a06ec172e65ff03ed0096
Robert Vásquez <vasquezrober09@gmail.com>
2023-06-12 17:41:23 -0400

    chore: :fire: move
    move files test to test and comment white list of cors

--- api/index.js
+++ api/index.js
@@ -9,25 +9,27 @@ const port = process.env.PORT || 3000;
 /** Use middleware express.json */
 app.use(express.json());
 
-const whiteList = [
-  'http://127.0.0.1:5500',
-  'http://127.0.0.1:8080',
-  'http://localhost:3000',
-  'https://express-rest-api-nu.vercel.app',
-  'https://express-rest-api-robert-ds.vercel.app'
-];
+// const whiteList = [
+//   'http://127.0.0.1:5500',
+//   'http://127.0.0.1:8080',
+//   'http://localhost:3000',
+//   'https://express-rest-api-nu.vercel.app',
+//   'https://express-rest-api-robert-ds.vercel.app'
+// ];
 
-const options = {
-  origin: (origin, callback) => {
-    if(whiteList.includes(origin)){
-      callback(null, true);
-    }else {
-      callback( new Error('Not permite'));
-    }
-  }
-};
+// const options = {
+//   origin: (origin, callback) => {
+//     if(whiteList.includes(origin)){
+//       callback(null, true);
+//     }else {
+//       callback( new Error('Not permite'));
+//     }
+//   }
+// };
 
-app.use(cors(options));
+// app.use(cors(options));
+
+app.use(cors());
 
 routerApi(app);
 app.use(logErrors);
