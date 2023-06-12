67a347777bf8b29f0f1f4f5d3cef334cf65d6df8
Robert Vásquez <vasquezrober09@gmail.com>
2023-06-12 16:50:18 -0400

    feat: :sparkles: cors
    implemets white list for address to cors nodjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes/index');
+const cors = require('cors');
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 
 const app = express();
@@ -7,6 +8,18 @@ const port = 3000;
 
 /** Use middleware express.json */
 app.use(express.json());
+const whiteList = ['http://127.0.0.1:5500', 'http://127.0.0.1:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      callback(null, true);
+    }else {
+      callback( new Error('Not permite'));
+    }
+  }
+};
+
+app.use(cors(options));
 
 routerApi(app);
 app.use(logErrors);
